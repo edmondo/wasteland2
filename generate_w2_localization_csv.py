a64c6552dc4b4c4e5557754ac256d381cf669753
Edmondo Tommasina <edmondo.tommasina@gmail.com>
2014-10-20 10:51:33 +0200

    Make use of the index() method
    ... instead of the own written function.

--- generate_w2_localization_csv.py
+++ generate_w2_localization_csv.py
@@ -8,19 +8,6 @@ import sys
 import string
 import csv
 
-# Find column by name
-def find_column_my_name(row, name):
-
-    cur_col = 0
-
-    while cur_col < len(row):
-        if row[cur_col] == name:
-            return cur_col
-        cur_col += 1
-
-    return -1
-
-
 # Open csv file
 with open(str(sys.argv[1]), 'rb') as f:
     reader = csv.reader(f)
@@ -30,8 +17,8 @@ with open(str(sys.argv[1]), 'rb') as f:
     for row in reader:
         rownum += 1
         if rownum == 0:
-            pos_key = find_column_my_name(row, 'Key')
-            pos_tra = find_column_my_name(row, 'it')
+            pos_key = row.index('Key')
+            pos_tra = row.index('it')
 
             if pos_key == -1:
                 print('Key column not found')
