34004d293725ec7fcf531e0abad4f094626f63ba
Edmondo Tommasina <edmondo.tommasina@gmail.com>
2014-11-06 23:22:30 +0100

    generate_w2_localization_it.py: extract only the it column

---
+++ generate_w2_localization_it.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python2
+#
+# Extract the italian localization for Wasteland 2
+#
+# 2014, Edmondo Tommasina
+
+import sys
+import string
+import csv
+
+# Open csv file
+with open(str(sys.argv[1]), 'rb') as f:
+    reader = csv.reader(f)
+
+    try:
+        # Get the column positions from the header line
+        row = reader.next()
+        pos_tra = row.index('it')
+    except ValueError:
+        print('Key or Translation column not found')
+    else:
+        # Iterate trough all rows
+        for row in reader:
+            # Print key and translation lines
+            print(row[pos_tra])
